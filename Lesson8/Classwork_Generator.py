5c339d5149716c537781f202fc795dbf0c16a7e1
Vitaliy Shchadnykh <86715642+lilvital@users.noreply.github.com>
2023-08-18 19:28:17 +0300

    Delete Lesson8 directory

--- Lesson8/Classwork_Generator.py
+++
@@ -1,8 +0,0 @@
-def get_square(limit):
-    num = 1
-    while num <= limit:
-        yield num ** 2
-        num += 1
-
-for el in get_square(5):
-    print(el)
--- Lesson8/Homework.py
+++
@@ -1,9 +0,0 @@
-def generator_numbers(start_num, end_num):
-    current_num = start_num
-    while current_num <= end_num:
-        yield current_num
-        current_num += 1
-
-
-for num in generator_numbers(1, 5):
-    print(num)
