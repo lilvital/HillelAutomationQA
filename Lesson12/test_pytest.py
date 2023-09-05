83f5d9a57276b08706745528b972b294d8e66356
Vitaliy <v.shchadnykh@gmail.com>
2023-09-05 20:01:42 +0300

    pytest_classwork

---
+++ Lesson12/test_pytest.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.mark.parametrize("first_num, second_num, expected_num", [(1,2,3),(2,2,4),(5,5,11)])
+def test_check_sum(first_num, second_num, expected_num):
+    assert custom_sum(first_num, second_num) == expected_num
+
+
+
+def custom_sum(first_num,  second_num):
+    return first_num + second_num
