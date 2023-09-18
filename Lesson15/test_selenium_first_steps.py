d22903d7979efdb57dc86ffecce810a3243652e2
Vitaliy <v.shchadnykh@gmail.com>
2023-09-18 22:17:27 +0300

    Facade

--- Lesson15/test_selenium_first_steps.py
+++ Lesson15/test_selenium_first_steps.py
@@ -39,26 +39,5 @@ class TestRegistration(TestBase):
         self._driver.quit()
 
     def test_registration_test(self):
-        self._registration_facade.register_user("test", "testlastname", self.user_email, self.user_password, self.user_password)
+        self._registration_facade.register_user("Lol", "Lololol", self.user_email, self.user_password, self.user_password)
         assert self._registration_facade.check_is_user_logged_in()
-
-
-    def test_registration_1_test(self):
-        self._driver.implicitly_wait(3)
-        self._driver.get("https://guest:welcome2qauto@qauto2.forstudy.space/")
-        sign_up_button = self._driver.find_element(By.XPATH, "//button[text()='Sign up']")
-        sign_up_button.click()
-        name_field = self._driver.find_element(By.ID, "signupName")
-        name_field.send_keys("test")
-        last_name_field = self._driver.find_element(By.ID, "signupLastName")
-        last_name_field.send_keys("lastnametest")
-        email_field = self._driver.find_element(By.ID, "signupEmail")
-        email_field.send_keys(self.user_email)
-        password_field = self._driver.find_element(By.ID, "signupPassword")
-        password_field.send_keys(self.user_password)
-        repeat_password_field = self._driver.find_element(By.ID, "signupRepeatPassword")
-        repeat_password_field.send_keys(self.user_password)
-        register_button = self._driver.find_element(By.XPATH, '//button[text()="Register"]')
-        register_button.click()
-        empty_garage = self._driver.find_elements(By.XPATH, "//p[text()='You don’t have any cars in your garage']")
-        assert len(empty_garage) != 0
